f5d3dfaf4ebd120222a56124bfcdba956b58f55f
Stav Gina <Stav.Gina@s.afeka.ac.il>
2026-06-22 17:50:16 +0300

    Updated env script to show important variables

--- scripts/env_variables.sh
+++ scripts/env_variables.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-printenv
+echo "=== Important Environment Variables ==="
+echo "Current User: $USER"
+echo "Home Directory: $HOME"
+echo "Current Shell: $SHELL"
+echo "System PATH: $PATH"
